1802844d1901b64c2a8dd287bf73a93a44fb16f6
Lucas <lucas@example.com>
2025-10-23 17:44:33 -0300

    chore: initial commit

---
+++ src/main/java/com/lucas/apitarefas/ApitarefasApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.apitarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitarefasApplication.class, args);
+	}
+
+}
